28826f0792f365c70a6c4ab385d6c87339293255
mootattar <mootattar@gmail.com>
2025-02-24 08:48:28 +0300

    fix the number issue when user dosn't login

--- app/_components/PostDetails.tsx
+++ app/_components/PostDetails.tsx
@@ -7,7 +7,7 @@ import { Post, PostContext } from "../contexts/PostContext";
 import noImage from "../assets/noImage.jpg";
 import Link from "next/link";
 import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "@/lib/firebase";
+import { db } from "@/lib/firebase";
 
 export default function PostDetails({ id }: { id: string }) {
   const { posts } = useContext(PostContext);
@@ -15,8 +15,7 @@ export default function PostDetails({ id }: { id: string }) {
   const [phone, setPhone] = useState("");
   useEffect(() => {
     const getUserPhone = async () => {
-      const user = auth.currentUser;
-      if (post && user && user.uid && post.uid) {
+      if (post && post.uid) {
         const userDoc = await getDoc(doc(db, "users", post.uid!));
         setPhone(userDoc.data()?.phone || post.phone);
       }
@@ -44,7 +43,7 @@ export default function PostDetails({ id }: { id: string }) {
               <div className="flex flex-col gap-2">
                 <p>تواصل مع الناشر من خلال الواتس آب</p>
                 <Link
-                  href={`https://wa.me/+962${phone.slice(1)}`}
+                  href={`https://wa.me/+962${phone?.slice(1)}`}
                   target="_blank"
                   className="text-white bg-green-500 text-nowrap hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all"
                 >
@@ -52,7 +51,7 @@ export default function PostDetails({ id }: { id: string }) {
                 </Link>
               </div>
               <Link
-                href={`https://wa.me/+962${phone.slice(1)}`}
+                href={`https://wa.me/+962${phone?.slice(1)}`}
                 target="_blank"
               >
                 {" "}
